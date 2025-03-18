8c884076a87f8e16dd7ef65cc80e4bc3a71b2a09
Shreyas <shreyastungar762@gmail.com>
2025-03-18 22:22:33 +0530

    vercel.json file added on backend also frontend base route utils code simplified

--- frontend/src/utils/apiConfig.js
+++ frontend/src/utils/apiConfig.js
@@ -1,12 +1,1 @@
-const isProd = process.env.NODE_ENV === "production";
-
-// Set the base URLs (removing the duplicate /api from production URL)
-const API_BASE_URL = isProd
-  ? "https://blogifybackend-sable.vercel.app"
-  : "https://blogifybackend-sable.vercel.app"
-//   : "http://localhost:8080";
-
-// Export the base URL directly
-export const BASE_URL = API_BASE_URL;
-
-export default BASE_URL;
+export const BASE_URL = "https://blogifybackend-sable.vercel.app";
